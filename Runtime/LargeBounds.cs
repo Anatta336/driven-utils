21cd0bd33b6e06bf2fc575c21fae0f9ad1dc724d
Anatta336 <sdriver@gmx.co.uk>
2020-06-29 13:17:05 +0100

    LargeBounds to bypass culling

---
+++ Runtime/LargeBounds.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace SamDriver.Util
+{
+    [ExecuteInEditMode]
+    [RequireComponent(typeof(MeshFilter))]
+    /// <summary>
+    /// Sets the bounds of the mesh used by MeshFilter to be very large,
+    /// effectively disabling Unity's basic mesh culling.
+    /// </summary>
+    public class LargeBounds : MonoBehaviour
+    {
+        void OnEnable()
+        {
+            var mesh = GetComponent<MeshFilter>().sharedMesh;
+            mesh.bounds = new Bounds(Vector3.zero, new Vector3(1000f, 1000f, 1000f));
+        }
+    }
+}
