5eeb625f7e597395cda86ea1c83ff684e828c1ad
Anatta336 <sdriver@gmx.co.uk>
2020-06-29 13:17:30 +0100

    UniformScale

---
+++ Runtime/UniformScale.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace SamDriver.Util
+{
+    [ExecuteInEditMode]
+    public class UniformScale : MonoBehaviour
+    {
+        public float Scale = 1f;
+        Vector3 scaleVector;
+
+        void OnEnable()
+        {
+            scaleVector = new Vector3(Scale, Scale, Scale);
+        }
+
+        void OnValidate() => ApplyScale();
+        void Update() => ApplyScale();
+
+        void ApplyScale()
+        {
+            scaleVector.Set(Scale, Scale, Scale);
+            transform.localScale = scaleVector;
+        }
+    }
+}
