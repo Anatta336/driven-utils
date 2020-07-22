532ba0564dac2928923b95530df305c9040e935a
Anatta336 <sdriver@gmx.co.uk>
2020-07-22 11:38:25 +0100

    avoid NaN

--- Runtime/Physics/AngularVelocity.cs
+++ Runtime/Physics/AngularVelocity.cs
@@ -7,6 +7,11 @@ namespace SamDriver.Util
         public static Quaternion RotationFromAngularVelocityRadians(Vector3 angularVelocityRadians)
         {
             float theta = angularVelocityRadians.magnitude;
+            if (theta == 0f)
+            {
+                return Quaternion.identity;
+            }
+
             Vector3 axisOfRotation = angularVelocityRadians / theta;
 
             float sinThetaOverTwo = Mathf.Sin(theta / 2f);
@@ -34,6 +39,34 @@ namespace SamDriver.Util
         {
             Vector3 angularVelocityDegrees = new Vector3(xDegrees, yDegrees, zDegrees);
             return RotationFromAngularVelocityDegrees(angularVelocityDegrees);
+        }
+
+        public static Vector3 AngularVelocityRadiansFromQuaternion(Quaternion rotation)
+        {
+            float thetaOverTwo = Mathf.Acos(rotation.w);
+            if (float.IsNaN(thetaOverTwo))
+            {
+                return Vector3.zero;
+            }
+
+            float sinThetaOverTwo = Mathf.Sin(thetaOverTwo);
+
+            if (sinThetaOverTwo == 0f)
+            {
+                return Vector3.zero;
+            }
+
+            Vector3 axisOfRotation = new Vector3(
+                rotation.x / sinThetaOverTwo,
+                rotation.y / sinThetaOverTwo,
+                rotation.z / sinThetaOverTwo
+            );
+            return axisOfRotation * (thetaOverTwo * 2f);
+        }
+
+        public static Vector3 AngularVelocityDegreesFromQuaternion(Quaternion rotation)
+        {
+            return AngularVelocityRadiansFromQuaternion(rotation) * Mathf.Rad2Deg;
         }
     }
 }
