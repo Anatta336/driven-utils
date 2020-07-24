ff4ce25a3780a6381b752c5b6ad3384e312d152a
Anatta336 <sdriver@gmx.co.uk>
2020-07-24 16:15:03 +0100

    Modulo

---
+++ Runtime/Maths/Modulo.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace SamDriver.Util
+{
+    public static class Modulo
+    {
+        /// <summary>
+        /// Similar to (a % b) but performs modulo rather than remainder.
+        /// They'd give same result for positive values, but differ in negative.
+        /// This form is useful for, for example, looping around the index of an array.
+        /// </summary>
+        public static int Mod(int a, int b)
+        {
+            return ((a %= b) < 0) ? (a + b) : a;
+        }
+    }
+}
